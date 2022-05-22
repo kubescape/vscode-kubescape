238fd97ad2985d625bf2991db493175137f43003
David Delarosa <xdavidel@gmail.com>
2022-05-22 13:32:20 +0300

    Allow adding arguments to registered function

--- src/extension.ts
+++ src/extension.ts
@@ -23,7 +23,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	// Subscribe all the exported functions in kubescape.ts
 	for (let exportedFunc of Object.keys(kubescape)){
 		let fullFuncName = `kubescape.${exportedFunc}`
-		subscriptions.push(vscode.commands.registerCommand(fullFuncName, () => eval(`${fullFuncName}()`)))
+		subscriptions.push(vscode.commands.registerCommand(fullFuncName, (args) => eval(`${fullFuncName}(args)`)))
 	}
 
 	const kubescapeApi = KubescapeApi.instance
