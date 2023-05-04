2b14fb51756463b84b7eb4e77925f7dbe5047f60
Hollow Man <hollowman@opensuse.org>
2023-05-04 22:16:54 +0300

    Bump kubescape version into v2.3.1
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- src/Kubescape/globals.ts
+++ src/Kubescape/globals.ts
@@ -1,3 +1,3 @@
-export const PACKAGE_STABLE_BUILD = "v2.0.176";
+export const PACKAGE_STABLE_BUILD = "v2.3.1";
 export const ERROR_KUBESCAPE_NOT_INSTALLED = "Kubescape is not installed!";
 export const COMMAND_VIEW_CTRL_DOC = "kubescape.viewCtrlDoc";
